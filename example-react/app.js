af18d091cfeb54ba92e1a8dffbed2d96b2d17216
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-01-02 14:51:57 -0500

    inicializar aplicación básica de React y renderizar tres botones

--- example-react/app.js
+++ example-react/app.js
@@ -2,12 +2,34 @@ import React from "https://esm.sh/react";
 import ReactDOM from "https://esm.sh/react-dom/client";
 const appDomElement = document.getElementById("app");
 const root = ReactDOM.createRoot(appDomElement);
-const button = React.createElement(
+
+const button1 = React.createElement(
   "button",
   {
-    "data-id": 123,
+    "data-id": 173,
     class: "mi-boton",
   },
   "Me gusta"
 );
-root.render(button);
+
+const button2 = React.createElement(
+  "button",
+  {
+    "data-id": 103,
+    class: "mi-boton",
+  },
+  "Me gusta"
+);
+
+const button3 = React.createElement(
+  "button",
+  {
+    "data-id": 113,
+    class: "mi-boton",
+  },
+  "Me gusta"
+);
+
+// elemento padre que envolvera a los botones
+const div = React.createElement("div", null, [button1, button2, button3]);
+root.render(div);
